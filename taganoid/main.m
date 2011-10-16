c508e79db2d07d025666fd8695da8c1d99fc1265
weshays <weshays@gbdev.com>
2011-10-16 00:26:06 -0700

    Initial commit of xcode files.

---
+++ taganoid/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  taganoid
+//
+//  Created by Wes Hays on 10/15/11.
+//  Copyright Great Basin Solutions, LLC. 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
